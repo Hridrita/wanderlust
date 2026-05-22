55f3ff3dcf0991c26de2ff776c5076c47ea7773e
Hridrita <moonsaha721@gmail.com>
2026-05-22 19:46:21 +0600

    added banner , navbar and footer

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between bg-white p-5">
+      <ul className="flex gap-3">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Destination</Link>
+        </li>
+        <li>
+          <Link href={"/"}>My Bookings</Link>
+        </li>
+      </ul>
+
+      <div>
+        <Image
+          src="/assets/Wanderlast.png"
+          height={150}
+          width={150}
+          alt="logo"
+        ></Image>
+      </div>
+
+      <ul className="flex gap-3">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link href={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link href={"/SignUp"}>Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
