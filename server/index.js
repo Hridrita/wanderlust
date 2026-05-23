0c783242b856a103ef5468a3ffee408a498ccc4e
Hridrita <moonsaha721@gmail.com>
2026-05-23 23:36:31 +0600

    created a patch api

--- server/index.js
+++ server/index.js
@@ -50,7 +50,16 @@ async function run() {
 
       const result = await destionationCollection.findOne({_id: new ObjectId(id)})
       res.json(result);
-    })
+    });
+
+    app.patch('/destination/:id', async(req,res)=>{
+      const {id} = req.params
+      const updatedData = req.body
+      console.log(updatedData);
+      const result = await destionationCollection.updateOne({_id: new ObjectId(id)},{$set: updatedData}
+    )
+    res.json(result);
+    });
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
