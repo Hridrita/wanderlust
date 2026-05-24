5330f9751511a69c78932ad73bc78e7389ad989c
Hridrita <moonsaha721@gmail.com>
2026-05-24 15:22:57 +0600

    created a delete api to delete destination

--- server/index.js
+++ server/index.js
@@ -60,6 +60,12 @@ async function run() {
     )
     res.json(result);
     });
+
+    app.delete('/destination/:id',async(req,res)=>{
+      const {id} = req.params
+      const result = await destionationCollection.deleteOne({_id: new ObjectId(id)})
+      res.json(result)
+    });
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
