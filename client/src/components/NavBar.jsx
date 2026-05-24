8bcf4886b6a756ed428cf32043303b942bfca81a
Hridrita <moonsaha721@gmail.com>
2026-05-24 23:40:08 +0600

    added sign in authentication

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,27 +1,49 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import {Avatar, Button} from "@heroui/react";
+import {Person} from "@gravity-ui/icons";
 
 const NavBar = () => {
+  const { data: session, isPending, error, refetch } = authClient.useSession();
+
+  console.log(session);
+
+  const user = session?.user;
+  console.log(user);
+
   return (
     <nav className="flex justify-between items-center bg-white px-8 py-4 shadow-md sticky top-0 z-50">
-      
       <ul className="flex gap-6 font-medium text-gray-700">
         <li>
-          <Link href={"/"} className="hover:text-blue-600 transition-colors">Home</Link>
+          <Link href={"/"} className="hover:text-blue-600 transition-colors">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href={"/destinations"} className="hover:text-blue-600 transition-colors">Destination</Link>
+          <Link
+            href={"/destinations"}
+            className="hover:text-blue-600 transition-colors"
+          >
+            Destination
+          </Link>
         </li>
         <li>
-          <Link href={"/"} className="hover:text-blue-600 transition-colors">My Bookings</Link>
+          <Link href={"/"} className="hover:text-blue-600 transition-colors">
+            My Bookings
+          </Link>
         </li>
         <li>
-          <Link href={"/add-destination"} className="hover:text-blue-600 transition-colors">Add Destination</Link>
+          <Link
+            href={"/add-destination"}
+            className="hover:text-blue-600 transition-colors"
+          >
+            Add Destination
+          </Link>
         </li>
       </ul>
 
-      
       <div className="cursor-pointer">
         <Image
           src="/assets/Wanderlast.png"
@@ -31,19 +53,43 @@ const NavBar = () => {
         />
       </div>
 
-      
       <ul className="flex gap-6 items-center font-medium text-gray-700">
         <li>
-          <Link href={"/profile"} className="hover:text-blue-600 transition-colors">Profile</Link>
+          <Link
+            href={"/profile"}
+            className="hover:text-blue-600 transition-colors"
+          >
+            Profile
+          </Link>
+        </li>
+
+        { user ? <>
+        <li><Avatar>
+        <Avatar.Image alt="John Doe" src={user.image} />
+        <Avatar.Fallback className="bg-cyan-400 font-bold">{user.name.charAt(0).toUpperCase()}</Avatar.Fallback>
+      </Avatar></li>
+        <li>
+          <Button variant="danger" className="px-5 py-2  text-white rounded-full hover:bg-red-700 transition-all">logout</Button>
+        </li>
+        </> :
+          <>
+        <li>
+          <Link
+            href={"/login"}
+            className="hover:text-blue-600 transition-colors"
+          >
+            Login
+          </Link>
         </li>
         <li>
-          <Link href={"/login"} className="hover:text-blue-600 transition-colors">Login</Link>
-        </li>
-        <li>
-          <Link href={"/signup"} className="px-5 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-all">
+          <Link
+            href={"/signup"}
+            className="px-5 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-all"
+          >
             Sign Up
           </Link>
         </li>
+        </>}
       </ul>
     </nav>
   );
